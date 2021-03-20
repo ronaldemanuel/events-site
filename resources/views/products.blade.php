c9a909889152f128c27938c0530523a596cc2802
ronaldemanuel <emronald15el@gmail.com>
2021-03-20 00:17:19 -0300

    adding static files

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,7 +1,16 @@
-<h1>página de produtos</h1>
-<ul>
-    <li>Produto 1</li>
-    <li>Produto 2</li>
-    <li>Produto 3</li>
-</ul>
-<a href="/">Voltar</a>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>Document</title>
+
+        <link rel="stylesheet" href="/css/style.css">
+        <script src="/js/script.js"></script>
+    </head>
+    <body>
+        <h1>Tela de produtos</h1>
+        <img src="/img/event_placeholder.jpg" alt="">
+    </body>
+</html>
